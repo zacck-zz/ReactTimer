ab30e8e94e8e05f06c0df6b0b70934868c32b70d
Zacck <coderv63@gmail.com>
2016-07-29 10:14:12 +0200

    starting work on the time

--- app/components/Main.jsx
+++ app/components/Main.jsx
@@ -1,13 +1,12 @@
 var React = require('react');
-var Nav = require('Nav');
 
 var Main = (props) => {
   return (
     <div>
-      <Nav/>
       <div className="row">
         <div className="columns medium-6 large-4 small-centered">
           {/*render children here*/}
+          <p>Main.jsx Rendered</p>
           {props.children}
         </div>
       </div>
